77d05f53dc0b89a04303bcf7d28d741a46d1b609
HyunSang Park <me@hyunsang.dev>
2022-03-13 08:17:30 +0900

    Feat: 검증 단계를 다시 구성함.

---
+++ cmd/diary.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"log"
+	"time"
+
+	"github.com/dev-hyunsang/my-own-diary/config"
+	"github.com/dev-hyunsang/my-own-diary/database"
+	"github.com/dev-hyunsang/my-own-diary/model"
+	"github.com/fatih/color"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// Logined
+func HomeHandler(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &jwt.MapClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.GetEnv("SIGNING_KEY")), nil
+	})
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status":  fiber.StatusUnauthorized,
+			"message": "로그인이 되지 않았어요. 다시 시도해 주세요.",
+			"time":    time.Now(),
+		})
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	var user model.Users
+
+	db, err := database.ConnectionDB()
+	if err != nil {
+		log.Println(color.RedString("ERROR"), "Failed to Connection DataBase")
+		log.Println(err)
+	}
+	db.Where("uuid = ?", claims["uuid"]).First(&user)
+
+	return c.Status(200).JSON(fiber.Map{
+		"stauts":  200,
+		"message": "성공적으로 로그인 하였어요!",
+		"data":    &user,
+		"time":    time.Now(),
+	})
+}
