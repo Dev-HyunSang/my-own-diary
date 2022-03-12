3d7a460a4d30727876d29cd36a19a0e1cf1beb4f
HyunSang Park <me@hyunsang.dev>
2022-03-12 16:45:34 +0900

    Feat: 오류 해결, GoGetEnv를 사용함.

--- config/env.go
+++ config/env.go
@@ -1,8 +1,20 @@
 package config
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/joho/godotenv"
+)
 
 func GetEnv(env string) string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(color.RedString("ERROR"), "Failed to Load .env File")
+		log.Fatalln(err)
+	}
+
 	getEnv := os.Getenv(env)
 	return getEnv
 }
