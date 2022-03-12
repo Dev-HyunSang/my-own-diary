ed0c9bce470dbd844d9248544f55b90917f5699c
HyunSang Park <me@hyunsang.dev>
2022-03-12 16:52:38 +0900

    Feat: AutoMigreate 구조체 변경, 코드 구조 변경.

--- main.go
+++ main.go
@@ -2,32 +2,51 @@ package main
 
 import (
 	"log"
+	"strings"
 
+	"github.com/dev-hyunsang/my-own-diary/cmd"
+	"github.com/dev-hyunsang/my-own-diary/config"
 	"github.com/dev-hyunsang/my-own-diary/database"
 	"github.com/dev-hyunsang/my-own-diary/model"
-	"github.com/dev-hyunsang/my-own-diary/router"
 	"github.com/fatih/color"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	jwtware "github.com/gofiber/jwt/v3"
 )
 
 func main() {
-	app := fiber.New(fiber.Config{
-		AppName: "my own diary",
-	})
-
-	router.Middleware(app)
-
 	db, err := database.ConnectionDB()
 	if err != nil {
 		log.Println(color.RedString("ERROR"), "Failed DataBase Connection")
 		log.Fatalln(err.Error())
 	}
 
-	err = db.AutoMigrate(model.Login{}, model.Register{})
+	err = db.AutoMigrate(&model.Users{})
 	if err != nil {
 		log.Println(color.RedString("ERROR"), "Failed to DataBase AutoMigrate")
 		log.Fatalln(err)
 	}
+
+	app := fiber.New(fiber.Config{
+		AppName: "my own diary",
+	})
+
+	app.Use(cors.New(cors.Config{
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodDelete,
+		}, ","),
+	}))
+
+	app.Post("/register", cmd.RegisterHandler)
+	app.Get("/login", cmd.LoginedIndexHandler)
+
+	user := app.Group("/user")
+	user.Use(jwtware.New(jwtware.Config{
+		SigningKey: config.GetEnv("SIGNING_KEY"),
+	}))
+	user.Get("/", cmd.LoginedIndexHandler)
 
 	if err = app.Listen(":3000"); err != nil {
 		log.Println(color.RedString("ERROR"), "Failed to Fiber Listen")
