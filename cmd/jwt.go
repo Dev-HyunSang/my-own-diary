41f1e2cedbc87945f5652c520eaff5c586ddf5be
HyunSang Park <me@hyunsang.dev>
2022-03-15 20:34:16 +0900

    Feat: JWT 검증에 대한 기능을 분리함.

--- cmd/jwt.go
+++ cmd/jwt.go
@@ -13,8 +13,8 @@ func CreateJWT(user *model.Users) (string, error) {
 
 	// Create the Claims
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer:    user.UUID.String(), // 로그인한 사용자의 UUID를 기록함.
-		IssuedAt:  time.Now().Unix(),  // time.Now을 사용할 경우 time.time으로 반환됨, unix를 사용하는 경우 int64로 반환함.
+		Issuer:    user.UUID,         // 로그인한 사용자의 UUID를 기록함.
+		IssuedAt:  time.Now().Unix(), // time.Now을 사용할 경우 time.time으로 반환됨, unix를 사용하는 경우 int64로 반환함.
 		ExpiresAt: exp,
 	})
 
@@ -22,4 +22,14 @@ func CreateJWT(user *model.Users) (string, error) {
 	token, err := claims.SignedString([]byte(config.GetEnv("SIGNING_KEY")))
 
 	return token, err
+}
+
+func VerificationJWT(cookie string) (*jwt.StandardClaims, error) {
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(config.GetEnv("SIGNING_KEY")), nil
+	})
+
+	clamis := token.Claims.(*jwt.StandardClaims)
+
+	return clamis, err
 }
