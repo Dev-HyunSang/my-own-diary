9e819f682ba18f76ea9cfcf578bb695dcea89ba0
HyunSang Park <me@hyunsang.dev>
2022-03-19 18:50:44 +0900

    Feat: 로그인 후 바로 일기를 확인할 수 있도록 개편

--- cmd/users.go
+++ cmd/users.go
@@ -4,31 +4,28 @@ import (
 	"log"
 	"time"
 
-	"github.com/dev-hyunsang/my-own-diary/config"
 	"github.com/dev-hyunsang/my-own-diary/database"
 	"github.com/dev-hyunsang/my-own-diary/model"
 	"github.com/fatih/color"
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v4"
 )
 
 // Logined
 func HomeHandler(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
-	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
-		return []byte(config.GetEnv("SIGNING_KEY")), nil
-	})
+	claims, err := VerificationJWT(cookie)
 	if err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"status":  fiber.StatusUnauthorized,
-			"message": "로그인이 되지 않았어요. 다시 시도해 주세요.",
+		c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  fiber.StatusInternalServerError,
+			"message": "Failed to Verification JWT",
 			"time":    time.Now(),
 		})
 	}
 
-	claims := token.Claims.(*jwt.StandardClaims)
-
-	var user model.Users
+	var (
+		diary []model.Diary
+		user  model.Users
+	)
 
 	db, err := database.ConnectionDB()
 	if err != nil {
@@ -38,10 +35,13 @@ func HomeHandler(c *fiber.Ctx) error {
 
 	db.Where("uuid = ?", claims.Issuer).First(&user)
 
+	db.Where("user_uuid = ?", claims.Issuer).Find(&diary)
+
 	return c.Status(200).JSON(fiber.Map{
 		"stauts":  200,
 		"message": "성공적으로 로그인 하였어요!",
 		"data":    &user,
+		"diary":   &diary,
 		"time":    time.Now(),
 	})
 }
